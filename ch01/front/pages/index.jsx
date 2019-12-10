211417b957f4c7b146f5439b4ff8b726281f1ade
wrkbr <zcarc1@gmail.com>
2019-12-10 22:29:56 +0900

    1-4. ant design 적용하기

--- ch01/front/pages/index.jsx
+++ ch01/front/pages/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Link from "next/link";
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
 
 const Home = () => {
 
     return(
         <>
-            {/*<a href="/about">about</a>*/}
-            <Link href="/about"><a>about</a></Link>
-            <div>Hello, Next!</div>
+            <Head>
+                <title>NodeBird</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.25.3/antd.css"/>
+            </Head>
+            <AppLayout>
+                <Link href="/about"><a>about</a></Link>
+                <div>Hello, Next!</div>
+            </AppLayout>
         </>
     );
 };
