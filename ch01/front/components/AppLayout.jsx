ada2e744b0063598644ee09c901284b1fc9f7acc
wrkbr <zcarc1@gmail.com>
2019-12-11 06:55:25 +0900

    1-7. 회원가입 state와 custom hook

--- ch01/front/components/AppLayout.jsx
+++ ch01/front/components/AppLayout.jsx
@@ -6,13 +6,13 @@ const AppLayout = ( {children} ) => {
     return (
         <div>
             <Menu mode="horizontal">
-                <Menu.Item key="home"><Link href="/">노드버드</Link></Menu.Item>
+                <Menu.Item key="home"><Link href="/"><a>노드버드</a></Link></Menu.Item>
                 <Menu.Item key="profile"><Link href="/profile"><a>프로필</a></Link></Menu.Item>
                 <Menu.Item key="mail">
                     <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
                 </Menu.Item>
             </Menu>
-            <Link href="/signup"><Button>회원가입</Button></Link>
+            <Link href="/signup"><a><Button>회원가입</Button></a></Link>
             {children}
         </div>
     );
--- ch01/front/pages/signup.jsx
+++ ch01/front/pages/signup.jsx
@@ -10,8 +10,29 @@ const Signup = () => {
     const [password, setPassword] = useState('');
     const [passwordCheck, setPasswordCheck] = useState('');
     const [term, setTerm] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [termError, setTermError] = useState(false);
 
-    const onSubmit = () => {};
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if(password !== passwordCheck) {
+            return setPasswordError(true);
+        }
+
+        if(!term) {
+            return setTermError(true);
+        }
+
+        console.log({
+            id,
+            nick,
+            password,
+            passwordCheck,
+            term
+        });
+        console.log('id: ', id);
+    };
     const onChangeId = (e) => {
         setId(e.target.value);
     };
@@ -22,11 +43,28 @@ const Signup = () => {
         setPassword(e.target.value);
     };
     const onChangePasswordCheck = (e) => {
+        setPasswordError(e.target.value !== password);
         setPasswordCheck(e.target.value);
     };
     const onChangeTerm = (e) => {
-        setTerm(e.target.value);
+        setTermError(false);
+        setTerm(e.target.checked);
     };
+
+    // 커스텀 Hook
+    // 만약 비밀번호 Input에 사용한다면
+    // value에 value가 들어가고 onChange에 handler가 들어간다.
+    const useInput = (initValue = null) => {
+        const [value, setter] = useState(initValue);
+        const handler = (e) => {
+            setter(e.target.value);
+        };
+        return [value, handler];
+    };
+
+    const [pwd, onChangePwd] = useInput('');
+
+
 
     return(
         <>
@@ -39,32 +77,34 @@ const Signup = () => {
                     <div>
                         <lable htmlFor="user-id">아이디</lable>
                         <br/>
-                        <Input name="user-id" required onChnage={onChangeId}></Input>
+                        <Input name="user-id" required onChange={onChangeId}></Input>
                     </div>
 
                     <div>
                         <lable htmlFor="user-nick">닉네임</lable>
                         <br/>
-                        <Input name="user-nick" required onChnage={onChangeNick}></Input>
+                        <Input name="user-nick" required onChange={onChangeNick}></Input>
                     </div>
 
                     <div>
                         <lable htmlFor="user-password">비밀번호</lable>
                         <br/>
-                        <Input name="user-password" type="password" value={password} required onChnage={onChangePassword}></Input>
+                        <Input name="user-password" type="password" value={password} required onChange={onChangePassword}></Input>
                     </div>
 
                     <div>
                         <lable htmlFor="user-password-check">비밀번호 체크</lable>
                         <br/>
-                        <Input name="user-password-check" type="password" value={passwordCheck} required onChnage={onChangePasswordCheck}></Input>
+                        <Input name="user-password-check" type="password" value={passwordCheck} required onChange={onChangePasswordCheck}></Input>
+                        {passwordError && <div style={{color: 'red'}}>비밀번호가 일치하지 않습니다.</div>}
                     </div>
 
                     <div>
-                        <Checkbox name="user-term" value={term} onChange={onChangeTerm}>동의합니다.</Checkbox>
+                        <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>동의합니다.</Checkbox>
+                        {termError && <div style={{color: 'red'}}>약관에 동의하셔야 합니다.</div>}
                     </div>
 
-                    <div>
+                    <div style={{marginTop: 10}}>
                         <Button type="primary" htmlType="submit">가입하기</Button>
                     </div>
                 </Form>
