87eb81504650a49982dbbebd9db1fd1540a30e4e
wrkbr <zcarc1@gmail.com>
2019-12-11 17:20:27 +0900

    2-3. antd 그리드 시스템

--- ch01/front/components/AppLayout.jsx
+++ ch01/front/components/AppLayout.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { Menu, Input, Button } from 'antd';
+import { Menu, Input, Button, Row, Col, Card, Avatar } from 'antd';
+
+const dummy = {
+    nickname: '이현수',
+    Post: [],
+    Followings: [],
+    Followers: [],
+};
 
 const AppLayout = ( {children} ) => {
     return (
@@ -14,7 +21,29 @@ const AppLayout = ( {children} ) => {
                 </Menu.Item>
             </Menu>
             <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-            {children}
+            <Row>
+                <Col xs={24} md={6}>
+                    <Card actions={[
+                        <div key="twit">짹짹<br/>{dummy.Post.length}</div>,
+                        <div key="following">팔로잉<br/>{dummy.Followings.length}</div>,
+                        <div key="follower">팔로워<br/>{dummy.Followers.length}</div>,
+                    ]}
+                    >
+                        <Card.Meta avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+                                   title={dummy.nickname}
+                        />
+                    </Card>
+                </Col>
+
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+
+                <Col xs={24} md={6}></Col>
+            </Row>
+
+
+
         </div>
     );
 };
