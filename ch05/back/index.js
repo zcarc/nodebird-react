54110dea851104e0c156d767f2b94d1125dd0c67
wrkbr <zcarc1@gmail.com>
2019-12-18 05:43:35 +0900

    5-2. HTTP 요청 주소 체계 이해하기

---
+++ ch05/back/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+// get() 의 첫번째 인자는 localhost:8080 뒤에 붙는 주소인데,
+// 프론트에서 백엔드로 '/' 으로 요청을 보내면
+// 두번째 인자인 콜백함수는 req, res 매개변수를 받고 res 객체로 응답을 보내준다.
+app.get('/', (req, res) => {
+   res.send('Hello, server');
+});
+
+app.get('/about', (req, res) => {
+   res.send('Hello, about');
+});
+
+// 파일이 실행되면 서버가 실행되는데
+// 실제 서버가 아니라 localhost 서버의 8080 포트번호의 서버
+app.listen(8080, () => {
+    console.log('server is running on http://localhost:8080');
+});
