1ae784bba1371266cb0aa9c7e5afda6f0ba9b99b
wrkbr <zcarc1@gmail.com>
2019-12-18 01:18:44 +0900

    4-15. 댓글 컴포넌트 만들기

--- ch05/front/components/PostCard.jsx
+++ ch05/front/components/PostCard.jsx
@@ -1,20 +1,48 @@
-import { Button, Card, Icon, Avatar } from 'antd';
+import { useState, useCallback } from 'react';
+import { Button, Card, Icon, Avatar, Form, Input, List, Comment } from 'antd';
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from "react-redux";
+import {ADD_COMMENT_REQUEST} from "../reducers/post";
 
 const PostCard = ( { post }) => {
-
     console.log('PostCard component...');
     console.log('post: ', post);
 
-    return (
+    const [commentFormOpended, setCommentFormOpened] = useState(false);
+    const [commentText, setCommentText] = useState('');
+    const { me } = useSelector(state => state.user);
+    const dispatch = useDispatch();
 
+    const onToggleComment = useCallback(() => {
+        setCommentFormOpened(prev => !prev);
+    }, []);
+
+    const onSubmitComment = useCallback( (e) => {
+        e.preventDefault();
+
+        if(!me) {
+            return alert('로그인이 필요합니다.');
+        }
+
+        return dispatch({
+           type: ADD_COMMENT_REQUEST,
+        });
+    }, []);
+
+    const onChangeCommentText = useCallback(() => {
+        setCommentText(e.target.value);
+    }, []);
+
+
+    return (
+        <div>
         <Card
             key={+post.createdAt}
             cover={post.img && <img alt="example" src={post.img} />}
             actions={[
                 <Icon type="retweet" key="retweet" />,
                 <Icon type="heart" key="heart" />,
-                <Icon type="message" key="message" />,
+                <Icon type="message" key="message" onClick={onToggleComment} />,
                 <Icon type="ellipsis" key="ellipsis" />,
             ]}
             extra={<Button>팔로우</Button>}
@@ -26,6 +54,37 @@ const PostCard = ( { post }) => {
             />
         </Card>
 
+            { commentFormOpended && (
+                <>
+                    <Form onSubmit={onToggleComment}>
+
+                        <Form.Item>
+                            <Input.TextArea rows={4} value={commentText} onChange={onChangeCommentText} />
+                        </Form.Item>
+
+                        <Button type="primary" htmlType="submit">삐약</Button>
+                    </Form>
+
+                    <List
+                        header={ `${post.Comments ? post.Comments.length : 0} 댓글` }
+                        itemLayout="horizontal"
+                        dataSource={post.Comment || []}
+                        renderItem={item => (
+                         <li>
+                             <Comment
+                                author={item.User.nickname}
+                                avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                                content={item.content}
+                                datetime={item.createdAt}
+                             />
+                         </li>
+                        )}
+                    />
+
+                </>
+            ) }
+
+        </div>
     );
 };
 
